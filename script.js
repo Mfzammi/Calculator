0682cc4c115869057876efe77a6b95b31b778715
Mfzammi <fadil.ahzami@2001Gmail.com>
2024-02-26 13:36:02 +0700

    WriteCommit

---
+++ script.js
@@ -0,0 +1,90 @@
+//inisiator
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+//function for every button
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+//function of operation
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if(previousOperator === null){
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if(buffer.length ===1){
+                buffer = '0';
+            }else{
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+//function of flush or remove operation
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer === "0"){
+        buffer = numberString;
+    }else{
+        buffer += numberString;
+    }
+}
+//button click function
+function init(){
+    document.querySelector('.cal-buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+init();
